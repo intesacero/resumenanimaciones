8deb686af52db063d94cad24ad55f6fee7eb38ff
Juan Carlos Ramiro <info@intes.es>
2012-04-01 11:24:08 +0200

    animacion vista2

--- resumenanimaciones/vista2.h
+++ resumenanimaciones/vista2.h
@@ -11,4 +11,22 @@
 @interface vista2 : UIViewController
 - (IBAction)irvista1:(id)sender;
 
+@property (nonatomic,retain) IBOutlet UIImageView *leafFalf;
+
+@property (nonatomic,retain) IBOutlet UIImageView *leafFalf1;
+
+@property (nonatomic,retain) IBOutlet UIImageView *leafFall2;
+
+@property (nonatomic,retain) IBOutlet UIImageView *leafFall3;
+
+@property (nonatomic,retain) IBOutlet UIImageView *leafFall4;
+
+
+
+-(void)animacionHojaCae;
+-(void)animacionHojaCae1;
+-(void)animacionHojaCae2;
+-(void)animacionHojaCae3;
+-(void)animacionHojaCae4;
+
 @end
--- resumenanimaciones/vista2.m
+++ resumenanimaciones/vista2.m
@@ -14,7 +14,21 @@
 
 @end
 
+
+
+
 @implementation vista2
+
+
+
+
+
+@synthesize leafFalf;
+@synthesize leafFalf1;
+@synthesize leafFall2;
+@synthesize leafFall3;
+@synthesize leafFall4;
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,8 +42,120 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self animacionHojaCae];
+    [self animacionHojaCae1];
+    [self animacionHojaCae2];
+    [self animacionHojaCae3];
+    [self animacionHojaCae4];
     // Do any additional setup after loading the view from its nib.
 }
+
+-(void)animacionHojaCae
+{
+    //Se puede crear una animacion customizada calculando la aceleracion o bien usar EaseIn
+    //que se basa en ir acelerando cada vez mas
+    [UIView animateWithDuration:2.0 
+                          delay:5.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^
+     {
+         
+         leafFalf.center=CGPointMake(leafFalf.center.x, 1024);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         
+     }
+     
+     ]; 
+    
+}
+
+
+-(void)animacionHojaCae1
+{
+    //Se puede crear una animacion customizada calculando la aceleracion o bien usar EaseIn
+    //que se basa en ir acelerando cada vez mas
+    [UIView animateWithDuration:2.0 
+                          delay:5.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^
+     {
+         
+         leafFalf1.center=CGPointMake(leafFalf1.center.x, 1024);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         
+     }
+     
+     ]; 
+    
+}
+-(void)animacionHojaCae2
+{
+    //Se puede crear una animacion customizada calculando la aceleracion o bien usar EaseIn
+    //que se basa en ir acelerando cada vez mas
+    [UIView animateWithDuration:1.0 
+                          delay:4.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^
+     {
+         
+         leafFall2.center=CGPointMake(leafFall2.center.x, 1024);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         
+     }
+     
+     ]; 
+    
+}
+-(void)animacionHojaCae3
+{
+    //Se puede crear una animacion customizada calculando la aceleracion o bien usar EaseIn
+    //que se basa en ir acelerando cada vez mas
+    [UIView animateWithDuration:1.0 
+                          delay:6.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^
+     {
+         
+         leafFall3.center=CGPointMake(leafFall3.center.x, 1024);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         
+     }
+     
+     ]; 
+    
+}
+
+-(void)animacionHojaCae4
+{
+    //Se puede crear una animacion customizada calculando la aceleracion o bien usar EaseIn
+    //que se basa en ir acelerando cada vez mas
+    [UIView animateWithDuration:1.0 
+                          delay:7.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^
+     {
+         
+         leafFall4.center=CGPointMake(leafFall4.center.x, 1024);
+     }      
+                     completion:^(BOOL finished) 
+     {
+         
+     }
+     
+     ]; 
+    
+}
+
+
+
 
 - (void)viewDidUnload
 {
@@ -48,4 +174,5 @@
     [self.navigationController pushViewController:v1 animated:YES];
     
 }
+
 @end
